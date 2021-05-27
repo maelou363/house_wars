124620d239625d95681f985a2dd02fbb12abe6eb
Lindsay Garza <lmwressell@aol.com>
2021-05-27 10:53:29 +0000

    testing for  bug with SendMail message not sending on live site

--- assets/js/sendMail.js
+++ assets/js/sendMail.js
@@ -1,14 +1,16 @@
-window.onload = function() {
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
-        event.preventDefault();
-        // generate a five digit number for the contact_number variable
-        this.contact_number.value = Math.random() * 100000 | 0;
-        // these IDs from the previous steps
-        emailjs.sendForm('contact_service', 'contact_form', this)
-            .then(function() {
-                console.log('SUCCESS!');
-            }, function(error) {
-                console.log('FAILED...', error);
-            });
-    });
+function sendMail(contactForm) {
+    emailjs.send("service_ecsqvze", "lindsay", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
 }
